f0f74e6fb365b06a6ea7fd5ffe7db33a5369bbf9
KESHAWN SHARPER <ksharper@studentmba.org>
2024-04-19 18:55:36 -0400

    rfd

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ function sendMail(){
      sender_last_name: document.querySelector("#sender_last_name").value,
      sender_phone_number:document.querySelector("#sender_phone_number").value,
      to: "awskeshawn@gmail.com",
-     client:"Cincinnati Drywall(No CLient Now)",
+     client:"Elyria, OH(No CLient Now)",
      sender_email: document.querySelector("#sender_email").value,
      message: document.querySelector("#message").value
  }
